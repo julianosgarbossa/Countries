67d94894f531a16b94ae3494aad283fc9be0ec01
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-03-17 14:16:15 -0300

    Criação e configuração incial do projeto

---
+++ Countries/Features/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Juliano Sgarbossa on 17/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

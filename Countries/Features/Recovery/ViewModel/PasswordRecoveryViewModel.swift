9b2ee8d5f9b17efbb6ba9ca3a35625e38b9b1b04
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-04-13 18:17:44 -0300

    refactor: adicionando validação ao campo de texto do email

--- Countries/Features/Recovery/ViewModel/PasswordRecoveryViewModel.swift
+++ Countries/Features/Recovery/ViewModel/PasswordRecoveryViewModel.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+protocol PasswordRecoveryViewModelDelegate: AnyObject {
+    func didValidateFieldAndButton(isValid: Bool)
+    func didRecoverySuccess()
+}
+
 final class PasswordRecoveryViewModel {
     
+    weak var delegate: PasswordRecoveryViewModelDelegate?
+    
+    private var email: String?
+    
+    func updateFieldAndButton(value: String?) {
+        email = value
+        delegate?.didValidateFieldAndButton(isValid: InputValidator.validateEmail(text: email))
+    }
+    
+    func passwordRecovery() {
+        // Integração futura com camada de serviço (Firebase Auth)
+        delegate?.didRecoverySuccess()
+    }
 }

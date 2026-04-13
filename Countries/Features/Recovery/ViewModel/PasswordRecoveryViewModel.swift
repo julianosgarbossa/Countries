09f7559ea15970d28c9659a0f94a3b080b77fa80
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-04-13 08:50:12 -0300

    feat: tela de recuperar senha e ajustes de imagem na pasta resource

---
+++ Countries/Features/Recovery/ViewModel/PasswordRecoveryViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PasswordRecoveryViewModel.swift
+//  Countries
+//
+//  Created by Juliano Sgarbossa on 13/04/26.
+//
+
+import Foundation
+
+final class PasswordRecoveryViewModel {
+    
+}

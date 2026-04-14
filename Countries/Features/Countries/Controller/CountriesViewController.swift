2c93a8c6478411a9a1199988767b8109762e12fb
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2026-04-14 16:18:13 -0300

    Movendo objeto de lista de países para a ViewModel e corrigo problemas

--- Countries/Features/Countries/Controller/CountriesViewController.swift
+++ Countries/Features/Countries/Controller/CountriesViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class CountriesViewController: UIViewController {
     
     private var countriesScreen: CountriesScreen?
+    private let countriesViewModel = CountriesViewModel()
+    
+    private var selectedIndex: Int = 0
+    
     private var continentList: [Continent] = [Continent(name: "Todas", isSelected: true),
                                               Continent(name: "África"),
                                               Continent(name: "América"),
@@ -18,98 +22,6 @@ class CountriesViewController: UIViewController {
                                               Continent(name: "Oceania"),
                                               Continent(name: "Antártida"),
     ]
-    private var countrieList: [Countrie] = [Countrie(name: "Brasil",
-                                                     capital: "Brasília",
-                                                     region: Region(name: "América do Sul"),
-                                                     continent: Continent(name: "América"),
-                                                     area: "8.515.767",
-                                                     borders: ["Argentina",
-                                                               "Bolívia",
-                                                               "Colômbia",
-                                                               "Guiana",
-                                                               "Guiana Francesa",
-                                                               "Paraguai",
-                                                               "Peru",
-                                                               "Suriname",
-                                                               "Uruguai",
-                                                               "Venezuela"],
-                                                     languages: ["Português"],
-                                                     population: "213.421.037",
-                                                     coin: "R$",
-                                                     flag: "br",
-                                                     isFavorited: false),
-                                            Countrie(name: "Argentina",
-                                                    capital: "Buenos Aires",
-                                                     region: Region(name: "América do Sul"),
-                                                     continent: Continent(name: "América"),
-                                                     area: "2.780.400",
-                                                     borders: ["Brasil",
-                                                               "Chile",
-                                                               "Paraguai",
-                                                               "Bolívia",
-                                                               "Uruguai"],
-                                                     languages: ["Espanhol",
-                                                                 "Guarani"],
-                                                     population: "45.808.747",
-                                                     coin: "$",
-                                                     flag: "ar",
-                                                     isFavorited: false),
-                                            Countrie(name: "Canadá",
-                                                     capital: "Ottawa",
-                                                     region: Region(name: "América do Norte"),
-                                                     continent: Continent(name: "América"),
-                                                     area: "9.984.670",
-                                                     borders: ["Estados Unidos"],
-                                                     languages: ["Inglês",
-                                                                 "Francês"],
-                                                     population: "38.246.108",
-                                                     coin: "$",
-                                                     flag: "ca",
-                                                     isFavorited: false),
-                                            Countrie(name: "Espanha",
-                                                     capital: "Madrid",
-                                                     region: Region(name: "Europa Ocidental"),
-                                                     continent: Continent(name: "Europa"),
-                                                     area: "505.990",
-                                                     borders: ["Portugal",
-                                                               "França",
-                                                               "Andorra",
-                                                               "Marrocos"],
-                                                     languages: ["Espanhol"],
-                                                     population: "47.615.034",
-                                                     coin: "€",
-                                                     flag: "es",
-                                                     isFavorited: false),
-                                            Countrie(name: "Itália",
-                                                     capital: "Roma",
-                                                     region: Region(name: "Europa Meridional"),
-                                                     continent: Continent(name: "Europa"),
-                                                     area: "301.340",
-                                                     borders: ["França",
-                                                               "Suíça",
-                                                               "Áustria",
-                                                               "Eslovênia",
-                                                               "San Marino",
-                                                               "Vaticano"],
-                                                     languages: ["Italiano"],
-                                                     population: "58.870.762",
-                                                     coin: "€",
-                                                     flag: "it",
-                                                     isFavorited: false),
-                                            Countrie(name: "Japão",
-                                                     capital: "Tokyo",
-                                                     region: Region(name: "Leste Asiático"),
-                                                     continent: Continent(name: "Ásia"),
-                                                     area: "377.975",
-                                                     borders: ["Nenhuma"],
-                                                     languages: ["Japonês"],
-                                                     population: "125.836.021",
-                                                     coin: "¥",
-                                                     flag: "jp",
-                                                     isFavorited: false)
-    ]
-    
-    private var selectedIndex: Int = 0
     
     override func loadView() {
         countriesScreen = CountriesScreen()
@@ -176,32 +88,32 @@ extension CountriesViewController: UICollectionViewDelegateFlowLayout {
 
 extension CountriesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let countrieDetailViewController = CountrieDetailViewController(countrie: countrieList[indexPath.row])
+        let countrieDetailViewController = CountrieDetailViewController(countrie: countriesViewModel.country(at: indexPath.row))
         navigationController?.pushViewController(countrieDetailViewController, animated: true)
     }
 }
 
 extension CountriesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countrieList.count
+        return countriesViewModel.numberOfRowsInSection
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CountrieTableViewCell.identifier, for: indexPath) as? CountrieTableViewCell else { return UITableViewCell() }
-        cell.setupCell(countrie: countrieList[indexPath.row])
+        cell.setupCell(countrie: countriesViewModel.country(at: indexPath.row))
         cell.delegate(delegate: self)
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(106)
+        return countriesViewModel.heightForRowAt
     }
 }
 
 extension CountriesViewController: CountrieTableViewCellDelegate {
     func countryCellDidTapFavorite(cell: CountrieTableViewCell) {
         guard let indexPath = countriesScreen?.countriesTableView.indexPath(for: cell) else { return }
-        countrieList[indexPath.row].isFavorited.toggle()
+        countriesViewModel.toggleFavorite(at: indexPath.row)
         countriesScreen?.countriesTableView.reloadRows(at: [indexPath], with: .none)
     }
 }
